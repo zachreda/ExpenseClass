d4b3781a56797f504b6b158dd2a852a869596e16
Zach Reda <zachreda23@gmail.com>
2020-09-08 15:59:38 -0400

    Initial Commit

---
+++ ExpenseClass/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ExpenseClass
+//
+//  Created by Zach Reda on 9/8/20.
+//  Copyright © 2020 Zach Reda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
